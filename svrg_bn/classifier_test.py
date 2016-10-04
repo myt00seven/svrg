fix naming file name with gradient

svrg_bn/classifier_test.py
@@ -35,12 +35,12 @@ def main(model=MODEL,gradient = GRADIENT, n_epochs=NUM_EPOCHS, n_hidden = NUM_HI
 
     models = {}
     if gradient == "svrg" or gradient == "all":
-        models.update({ 'svrg_classif': (custom_svrg1, {'learning_rate': 0.01, 'm': 50}) }) 
+        models.update({ 'svrg': (custom_svrg1, {'learning_rate': 0.01, 'm': 50}) }) 
     if gradient == "stream" or gradient == "all": # It is StreamingSVRG
-        models.update({ 'streaming_svrg_classif': (custom_streaming_svrg1, {'learning_rate': 0.01, 'm': 50, 'k_s_0': 1.0, 'k_s_ratio':1.003}) })
+        models.update({ 'streaming': (custom_streaming_svrg1, {'learning_rate': 0.01, 'm': 50, 'k_s_0': 1.0, 'k_s_ratio':1.003}) })
         #k_s is the ratio of how many batches are used in this iteration of StreamingSVRG
     if gradient == "adagrad" or gradient == "all":
-        models.update( { 'adagrad_classif': (custom_adagrad, {'learning_rate': 0.01, 'eps': 1.0e-8}) })
+        models.update( { 'adagrad': (custom_adagrad, {'learning_rate': 0.01, 'eps': 1.0e-8}) })
 
     # print(models.keys())
 
@@ -52,13 +52,14 @@ def main(model=MODEL,gradient = GRADIENT, n_epochs=NUM_EPOCHS, n_hidden = NUM_HI
 
     for model in models.keys():
         update, update_params = models[model]
+
         np.random.seed(19921010)
 
         network = neuralclassifier.NeuralClassifier(n_input=X_train.shape[1], n_hidden=n_hidden, n_output=10)
 
         train_err, val_err = network.train(X_train, y_train, X_val, y_val, X_test, y_test,
                                            n_epochs=n_epochs, lambd=0.0,
-                                           objective=objective, update=update, batch_size=BATCH_SIZE, gradient=GRADIENT, **update_params )
+                                           objective=objective, update=update, batch_size=BATCH_SIZE, gradient=model, **update_params )
 
     #     np.savez('models/model_%s.npz' % model, *lasagne.layers.get_all_param_values(network.output_layer))
     #     np.savez('models/model_%s_val_error.npz' % model, val_err)