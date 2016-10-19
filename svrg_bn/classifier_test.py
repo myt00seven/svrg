d030622a1e621335872c89b3584707a816c8c6ef
myt00seven <myt007@gmail.com>
2016-10-19 12:45:00 -0500

    l1 lambda 0.1 lr 0.01

--- svrg_bn/classifier_test.py
+++ svrg_bn/classifier_test.py
@@ -41,7 +41,7 @@ def main(model=MODEL,gradient = GRADIENT, n_epochs=NUM_EPOCHS, n_hidden = NUM_HI
         models.update({ 'streaming': (custom_streaming_svrg1, {'learning_rate': 0.1, 'm': 50, 'k_s_0': 1.0, 'k_s_ratio':1.03}) })
         #k_s is the ratio of how many batches are used in this iteration of StreamingSVRG
     if gradient == "adagrad" or gradient == "all":
-        models.update( { 'adagrad': (custom_adagrad, {'learning_rate': 0.1, 'eps': 1.0e-8}) })
+        models.update( { 'adagrad': (custom_adagrad, {'learning_rate': 0.01, 'eps': 1.0e-8}) })
 
     # print(models.keys())
 
@@ -59,7 +59,7 @@ def main(model=MODEL,gradient = GRADIENT, n_epochs=NUM_EPOCHS, n_hidden = NUM_HI
         network = neuralclassifier.NeuralClassifier(n_input=X_train.shape[1], n_hidden=n_hidden, n_output=10)
 
         train_err, val_err = network.train(X_train, y_train, X_val, y_val, X_test, y_test,
-                                           n_epochs=n_epochs, lambd=0.01,
+                                           n_epochs=n_epochs, lambd=0.1,
                                            objective=objective, update=update, batch_size=BATCH_SIZE, gradient=model,  **update_params )
 
     #     np.savez('models/model_%s.npz' % model, *lasagne.layers.get_all_param_values(network.output_layer))
