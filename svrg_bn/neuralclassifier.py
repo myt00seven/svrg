1c8ac81cfab21c8d2b0d266ec95a4bac463ac540
myt00seven <myt007@gmail.com>
2016-11-09 13:01:28 -0600

    minor bug fix

--- svrg_bn/neuralclassifier.py
+++ svrg_bn/neuralclassifier.py
@@ -49,7 +49,7 @@ class NeuralClassifier:
 
         if (gradient == 'svrg'):
             loss = objective(prediction, self.target_var) + 0.01 * l2_reg
-        elif (gradient == 'stream'):
+        elif (gradient == 'streaming'):
             loss = objective(prediction, self.target_var) + 0.1 * l1_reg        
         elif (gradient == 'adagrad'):
             loss = objective(prediction, self.target_var) + 0.1 * l1_reg        
